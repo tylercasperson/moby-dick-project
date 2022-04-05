d1459e4ec318cb5650b28556c4105d26f5302ad3
Tyler Casperson <tylercasperson@gmail.com>
2022-04-04 23:07:02 -0600

    added input field component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import InputLabel from './components/layout/InputLabel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '700px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        verticalAlign: 'middle',
+      }}
+    >
+      <InputLabel label={'File Location'} />
     </div>
   );
 }
---
+++ src/components/layout/InputLabel.js
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+
+const InputLabel = (props) => {
+  const labelText = useRef('');
+  const inputText = useRef('');
+
+  const onFocus = (ref) => {
+    ref.current.classList.add('active');
+  };
+
+  const onBlur = (ref, input) => {
+    if (input.current.value === '') {
+      ref.current.classList.remove('active');
+    }
+  };
+
+  return (
+    <div style={{ position: 'relative', margin: '40px 0' }}>
+      <label ref={labelText} htmlFor='inputText'>
+        {props.label}
+      </label>
+      <input
+        onFocus={() => onFocus(labelText)}
+        onBlur={() => onBlur(labelText, inputText)}
+        ref={inputText}
+        inputText
+        type='text'
+        id='inputText'
+      />
+    </div>
+  );
+};
+
+export default InputLabel;
