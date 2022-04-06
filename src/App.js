7517b9c8f379205f492f1a666ac79981f2c83d1d
Tyler Casperson <tylercasperson@gmail.com>
2022-04-05 19:55:03 -0600

    added a ripple button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import InputLabel from './components/layout/InputLabel';
+import RippleButton from './components/layout/RippleButton';
 
 function App() {
   return (
-    <div
-      style={{
-        width: '700px',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        verticalAlign: 'middle',
-      }}
-    >
-      <InputLabel label={'File Location'} />
+    <div>
+      <div
+        style={{
+          width: '700px',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          verticalAlign: 'middle',
+        }}
+      >
+        <InputLabel label={'File Location'} />
+        <RippleButton />
+      </div>
     </div>
   );
 }
