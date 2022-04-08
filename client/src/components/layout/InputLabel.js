a428607cac4c907838672a8ddd7340f7b9d89a63
Tyler Casperson <tylercasperson@gmail.com>
2022-04-08 01:18:32 -0600

    added functionality on the input label

--- client/src/components/layout/InputLabel.js
+++ client/src/components/layout/InputLabel.js
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 const InputLabel = (props) => {
   const labelText = useRef('');
   const inputText = useRef('');
+
+  const [newWord, setNewWord] = useState('');
 
   const onFocus = (ref) => {
     ref.current.classList.add('active');
@@ -14,17 +16,30 @@ const InputLabel = (props) => {
     }
   };
 
+  const onEnter = (e) => {
+    if (inputText.current.value !== '') {
+      if (e.key === 'Enter') {
+        props.array.push(inputText.current.value);
+        setNewWord(inputText.current.value);
+        inputText.current.value = '';
+      }
+    }
+  };
+
   return (
-    <div style={{ position: 'relative', margin: '40px 0' }}>
+    <div style={{ position: 'relative', margin: '30px 0', width: '30vw' }}>
       <label ref={labelText} htmlFor='inputText'>
         {props.label}
       </label>
       <input
         onFocus={() => onFocus(labelText)}
         onBlur={() => onBlur(labelText, inputText)}
+        onKeyDown={(e) => onEnter(e)}
+        onKeyDownCapture={props.onEnter}
         ref={inputText}
         type='text'
         id='inputText'
+        new-word={newWord}
       />
     </div>
   );
